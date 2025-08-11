90a11966c3c88a2fd8880f684d75f3c5a3c9cab3
Julia Wolfram <julia.wolfram@gmx.at>
2025-08-11 20:50:29 +0200

    feat: fully functional ex00

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,20 +6,21 @@
 /*   By: jwolfram <jwolfram@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 17:35:36 by jwolfram          #+#    #+#             */
-/*   Updated: 2025/08/02 23:48:18 by jwolfram         ###   ########.fr       */
+/*   Updated: 2025/08/11 20:48:09 by jwolfram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-Fixed::Fixed()
+Fixed::Fixed(): _fixedPointValue()
 {
 	std::cout << "Default Constructor Called" << std::endl;
 }
 
-Fixed::Fixed(const Fixed &copy): _fixedPointValue(copy._fixedPointValue)
+Fixed::Fixed(const Fixed &copy)
 {
 	std::cout << "Copy Constructor Called" << std::endl;
+	_fixedPointValue = copy.getRawBits();
 }
 
 Fixed::~Fixed()
@@ -27,11 +28,12 @@ Fixed::~Fixed()
 	std::cout << "Destructor Called" << std::endl;
 }
 
-Fixed::operator=(const Fixed &other)
+Fixed &Fixed::operator=(const Fixed &other)
 {
-	if (this != &other)
-		_fixedPointValue = other._fixedPointValue;
 	std::cout << "Copy Assignment Operator Called" << std::endl;
+	if (this != &other)
+		_fixedPointValue = other.getRawBits();
+	return (*this);
 }
 
 int	Fixed::getRawBits() const
--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: jwolfram <jwolfram@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 17:35:58 by jwolfram          #+#    #+#             */
-/*   Updated: 2025/08/02 23:48:23 by jwolfram         ###   ########.fr       */
+/*   Updated: 2025/08/11 20:43:44 by jwolfram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ class Fixed
 {
 	private:
 		int					_fixedPointValue;
-		static const int	_fractionalBits;
+		static const int	_fractionalBits = 8;
 
 	public:
 		Fixed();
